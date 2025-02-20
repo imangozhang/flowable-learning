e310d81207e69e08f6234278d2ee1778699b587f
zhangbh <zhangbh@lenovocloud.com>
2025-02-20 18:45:17 +0800

    feat: init flowable

---
+++ src/main/java/com/baohou/App.java
@@ -0,0 +1,26 @@
+package com.baohou;
+
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+                .setJdbcUrl("jdbc:mysql://127.0.0.1:3306/flowable?zeroDateTimeBehavior=convertToNull&useUnicode=true" +
+                        "&characterEncoding=utf8&nullCatalogMeansCurrent=true")
+                .setJdbcUsername("root")
+                .setJdbcPassword("123456")
+                .setJdbcDriver("com.mysql.cj.jdbc.Driver")
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+
+        ProcessEngine processEngine = cfg.buildProcessEngine();
+        System.out.println( "Hello World!" );
+    }
+}
